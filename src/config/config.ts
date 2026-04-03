a5eb14b251bc2c788e23b9552df5dec10181b10e
Moody <bolaji100k@gmail.com>
2026-04-02 23:18:25 -0400

    [KAN2-11] Setup middleware configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,8 +7,11 @@ const MONGO_HOST = process.env.MONGO_HOST ?? "";
 const MONGO_DATABASE = process.env.MONGO_DATABASE ?? "";
 const MONGO_PORT = process.env.MONGO_PORT ?? "";
 const MONGO_URL = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+const JWT_SECRET = process.env.JWT_SECRET ?? "";
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN ?? "7d";
 
 export const config = {
   environment: ENVIRONMENT,
-  mongo: { url: MONGO_URL }
+  mongo: { url: MONGO_URL },
+  jwt: { secret: JWT_SECRET, expiresIn: JWT_EXPIRES_IN }
 };
