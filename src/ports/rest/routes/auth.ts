4453767555f26ca360a0b84617342c6601ba2da9
Moody <bolaji100k@gmail.com>
2026-04-05 00:56:51 -0400

    [KAN2-20] Add login route to auth routes

---
+++ src/ports/rest/routes/auth.ts
@@ -0,0 +1,38 @@
+import express, { NextFunction, Response, Request } from "express";
+import dependencies from "../../../infrastructure/dependencies";
+import { register, login } from "../../../controllers/auth";
+
+const router = express.Router();
+
+router.post("/register", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const result = await register(dependencies)({ name, email, password, role });
+    res.status(201).json({
+      message: "User registered successfully",
+      user: { id: result._id, name: result.name, email: result.email, role: result.role }
+    });
+  } catch (error) {
+    console.log(`Error in registering user: ${JSON.stringify((error as Error).message)}`);
+    res.status(400).json({
+      message: `${(error as Error).message}`
+    });
+  }
+});
+
+router.post("/login", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+    await login(dependencies)({ email, password });
+    res.status(200).json({
+      message: "Login successful"
+    });
+  } catch (error) {
+    console.log(`Error in login: ${JSON.stringify((error as Error).message)}`);
+    res.status(401).json({
+      message: `${(error as Error).message}`
+    });
+  }
+});
+
+export = router;
